203987bd9034a737a8a7e377e416373e3ef8e0d4
Paul Welsh <p.welsh@wearestagetwo.com>
2018-05-31 00:44:25 +0100

    remove insecure dependency

--- build/build.js
+++ build/build.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 var gulp = require('gulp');
 var runSequence = require('run-sequence').use(gulp);
 var environments = require('gulp-environments');
@@ -7,18 +7,8 @@ var gutil = require('gulp-util');
 var config = require('./config');
 
 var build = function(callback) {
-  runSequence(
-    'clean--all',
-    'scripts',
-    'styles',
-    'markup',
-    'assets',
-    'styleguide',
-    'lab',
-    'robots',
-    callback
-  );
-}
+  runSequence('clean--all', 'scripts', 'styles', 'markup', 'assets', 'styleguide', 'lab', 'robots', callback);
+};
 
 gulp.task('build', function(callback) {
   environments.current(environments.development);
@@ -33,12 +23,7 @@ gulp.task('production', function(callback) {
 gulp.task('watch', function() {
   gulp.watch(config.paths.styles.src + '**/*.scss', ['styles', 'assets']);
   gulp.watch(config.paths.scripts.src + '**/*.js', ['scripts']);
-  gulp.watch([
-    config.paths.lab.src + '**/*',
-    config.paths.views.src + '**/*',
-    config.paths.data.src + '**/*',
-    config.paths.styleguide.src + '**/*'
-  ], ['build']);
+  gulp.watch([config.paths.lab.src + '**/*', config.paths.views.src + '**/*', config.paths.data.src + '**/*', config.paths.styleguide.src + '**/*'], ['build']);
 });
 
 gulp.task('serve', ['build', 'watch'], function(callback) {
@@ -46,29 +31,26 @@ gulp.task('serve', ['build', 'watch'], function(callback) {
   var serveStatic = require('serve-static');
   var finalhandler = require('finalhandler');
   var ngrok = require('ngrok');
-  var open = require('open');
 
   var serve = serveStatic(config.basePaths.dist, {
-    "index": ['index.html', 'index.htm']
+    index: ['index.html', 'index.htm']
   });
 
   var server = http.createServer(function(req, res) {
     var done = finalhandler(req, res);
     serve(req, res, done);
-  })
+  });
 
-  var serverPort = config.serverPort ? config.serverPort : Math.floor((Math.random() * 1000) + 3000);
+  var serverPort = config.serverPort ? config.serverPort : Math.floor(Math.random() * 1000 + 3000);
   var localhost = 'http://localhost:' + serverPort;
 
   server.listen(serverPort, function() {
     gutil.log(gutil.colors.green('Local URL:'), 'http://localhost:' + serverPort);
     ngrok.connect(serverPort, function(error, url) {
       gutil.log(gutil.colors.green('External URL:'), url);
-      open(localhost, 'google chrome');
       callback();
     });
   });
-
 });
 
 gulp.task('default', ['build']);
